cbf09a1b2183442153458d1fb82285f1863058b7
Mahesh Kamble <kamble2682001@gmail.com>
2026-03-08 17:24:29 +0530

    Added Sum of Numbers code

---
+++ assignments/01-code/SumOfNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumOfNumbers {
+    public static void main(String[] args){
+        int a, b, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        a = sc.nextInt();
+        System.out.println("Enter the second number : ");
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is : " + sum);
+    }
+}
